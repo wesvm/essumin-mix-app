232eaee03d6bd07399af012404cb8c0b521a7a34
blue <samuel_walter_@hotmail.com>
2023-07-22 17:22:13 -0500

    add synonyms attribute

--- lib/data/option.dart
+++ lib/data/option.dart
@@ -1,10 +1,16 @@
 class Option {
   final String key;
   final String value;
+  final List<String>? synonyms;
 
-  Option(this.key, this.value);
+  Option(this.key, this.value, {this.synonyms});
 
   factory Option.fromJson(Map<String, dynamic> json) {
-    return Option(json['key'], json['value']);
+    return Option(
+      json['key'],
+      json['value'],
+      synonyms:
+          json['synonyms'] != null ? List<String>.from(json['synonyms']) : null,
+    );
   }
 }

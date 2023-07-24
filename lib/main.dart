d6679020faf485850e86957fec2eab605b13620a
blue <samuel_walter_@hotmail.com>
2023-07-24 01:58:21 -0500

    adding bottom navigator bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,32 @@
-import 'package:essumin_mix/data/option.dart';
-import 'package:essumin_mix/ui/screens/option_selected_screen.dart';
-import 'package:essumin_mix/ui/themes/custom_elevated_button.dart';
+import 'package:essumin_mix/ui/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'blocs/data_bloc.dart';
 import 'ui/screens/home_screen.dart';
+import 'package:essumin_mix/data/models/sigla/sigla.dart';
+import 'package:essumin_mix/data/models/simbologia/simbologia.dart';
+
+import 'package:essumin_mix/ui/screens/siglas/sigla_selected_screen.dart';
+import 'package:essumin_mix/ui/screens/simbologia/simbologia_selected_screen.dart';
+
+import 'package:essumin_mix/ui/themes/custom_elevated_button.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const ProfileScreen(username: 'Walter Vilca')
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -29,21 +43,51 @@ class MyApp extends StatelessWidget {
             ),
         elevatedButtonTheme: customElevatedButtonTheme,
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => BlocProvider(
-              create: (_) => DataBloc(),
-              child: const HomeScreen(),
+      home: Scaffold(
+        body: _screens[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
             ),
-        '/option': (context) {
+            BottomNavigationBarItem(
+              icon: Icon(Icons.push_pin),
+              label: 'Info',
+            ),
+          ],
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+        ),
+      ),
+      routes: {
+        // '/': (context) => BlocProvider(
+        //       create: (_) => DataBloc(),
+        //       child: const HomeScreen(),
+        //     ),
+        '/siglas': (context) {
           final Map<String, dynamic> routeArguments = ModalRoute.of(context)!
               .settings
               .arguments as Map<String, dynamic>;
           final String category = routeArguments['category'] as String;
-          final List<Option> options =
-              routeArguments['options'] as List<Option>;
+          final List<Sigla> options = routeArguments['options'] as List<Sigla>;
 
-          return OptionSelectedScreen(category: category, options: options);
+          return SiglaSelectedScreen(category: category, options: options);
+        },
+        '/simbologias': (context) {
+          final Map<String, dynamic>? routeArguments = ModalRoute.of(context)
+              ?.settings
+              .arguments as Map<String, dynamic>?;
+
+          final String category = routeArguments?['category'] as String;
+          final List<Simbologia> data =
+              routeArguments?['data'] as List<Simbologia>;
+
+          return SimbologiaSelectedScreen(category: category, data: data);
         },
       },
     );

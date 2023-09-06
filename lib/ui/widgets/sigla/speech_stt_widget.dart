9a0618a71a464083c6afae14d7732ac39e194974
blue <samuel_walter_@hotmail.com>
2023-09-05 23:37:27 -0500

    updated stt

--- lib/ui/widgets/sigla/speech_stt_widget.dart
+++ lib/ui/widgets/sigla/speech_stt_widget.dart
@@ -1,8 +1,9 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_error.dart';
 
 import 'package:speech_to_text/speech_recognition_result.dart';
-import 'package:speech_to_text/speech_to_text.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 class SpeechToTextWidget extends StatefulWidget {
   final String language;
@@ -19,68 +20,105 @@ class SpeechToTextWidget extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<SpeechToTextWidget> createState() => _SpeechToTextWidgetState();
+  State<SpeechToTextWidget> createState() => SpeechToTextWidgetState();
 }
 
-class _SpeechToTextWidgetState extends State<SpeechToTextWidget> {
-  final SpeechToText _speech = SpeechToText();
-  bool _isButtonPressed = false;
+class SpeechToTextWidgetState extends State<SpeechToTextWidget> {
+  late stt.SpeechToText _speech;
 
-  bool _speechEnabled = false;
+  bool speechAvailable = false;
+  bool _isListening = false;
   bool hasPermission = true;
+
   late String micText;
+  late String micList;
   late String localeId;
 
   @override
   void initState() {
     super.initState();
+    _speech = stt.SpeechToText();
     _initSpeech();
     _initLanguage();
   }
 
-  void _initSpeech() async {
-    _speechEnabled = await _speech.initialize();
-    hasPermission = await _speech.hasPermission;
+  Future<void> _initSpeech() async {
+    var hasPermission = await _speech.hasPermission;
+    if (!mounted) return;
+
+    setState(() {
+      hasPermission = hasPermission;
+    });
   }
 
   void _initLanguage() {
     if (widget.language == 'en') {
       localeId = 'en_US';
-      micText = 'Press and hold to speak.';
+      micText = 'Press to speak.';
+      micList = 'Listening..';
     } else {
       localeId = 'es_ES';
-      micText = 'Manten presionado para hablar.';
+      micText = 'Presiona para hablar.';
+      micList = 'Escuchando..';
     }
   }
 
-  void _startListening() async {
-    if (_speech.isListening) {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _initSpeech();
+  }
+
+  Future<void> _startListening() async {
+    if (_isListening || _speech.isListening) {
       _stopListening();
       return;
     }
 
-    if (!_speech.isListening) {
+    var speechAvailable = await _speech.initialize(
+      onStatus: (status) => debugPrint('status $status'),
+      onError: errorListener,
+    );
+
+    if (speechAvailable) {
       widget.textEditingController.clear();
       _handleTextChanged('');
-      await _speech.listen(
+      setState(() => _isListening = true);
+      _speech.listen(
         onResult: _onSpeechResult,
         localeId: localeId,
+        pauseFor: const Duration(seconds: 3),
+        cancelOnError: true,
       );
+    }
+  }
+
+  void errorListener(SpeechRecognitionError error) {
+    debugPrint('error $error');
+    debugPrint('mounted: $mounted');
+    if (mounted) {
+      if (error.permanent) cancelListening();
     }
   }
 
   void _onSpeechResult(SpeechRecognitionResult result) {
     widget.textEditingController.text = result.recognizedWords;
-
     _handleTextChanged(widget.textEditingController.text);
+    if (result.finalResult) setState(() => _isListening = false);
   }
 
-  void _stopListening() async {
-    await _speech.stop();
+  void _stopListening() {
+    setState(() => _isListening = false);
+    _speech.stop();
   }
 
   void _handleTextChanged(String text) {
     widget.onChanged(text.trim().isEmpty);
+  }
+
+  void cancelListening() {
+    setState(() => _isListening = false);
+    _speech.cancel();
   }
 
   @override
@@ -100,56 +138,32 @@ class _SpeechToTextWidgetState extends State<SpeechToTextWidget> {
               labelText: widget.labelText,
             ),
             onChanged: _handleTextChanged,
+            minLines: 1,
+            maxLines: null,
           ),
           const SizedBox(height: 16.0),
           if (hasPermission)
             Column(
               children: [
                 AvatarGlow(
-                  endRadius: 65.0,
-                  animate: _isButtonPressed,
+                  endRadius: 60.0,
+                  animate: _speech.isListening,
                   duration: const Duration(milliseconds: 2000),
                   repeat: true,
                   showTwoGlows: true,
-                  child: GestureDetector(
-                    onTapDown: (details) {
-                      if (_speechEnabled) {
-                        setState(() {
-                          _isButtonPressed = true;
-                        });
-                        _startListening();
-                      }
-                    },
-                    onTapUp: (details) {
-                      if (mounted) {
-                        setState(() {
-                          _isButtonPressed = false;
-                        });
-                      }
-                      _stopListening();
-                    },
-                    onTapCancel: () {
-                      if (mounted) {
-                        setState(() {
-                          _isButtonPressed = false;
-                        });
-                      }
-                      _stopListening();
-                    },
-                    child: CircleAvatar(
-                      radius: _isButtonPressed ? 35 : 30,
-                      backgroundColor:
-                          _isButtonPressed ? Colors.red : Colors.blue,
-                      child: Icon(
-                        !_isButtonPressed ? Icons.mic_off : Icons.mic,
-                        size: 30,
-                        color: Colors.white,
-                      ),
+                  child: FloatingActionButton(
+                    onPressed: _startListening,
+                    backgroundColor:
+                        _speech.isListening ? Colors.red : Colors.blue,
+                    child: Icon(
+                      !_isListening ? Icons.mic_off : Icons.mic,
+                      size: 30,
+                      color: Colors.white,
                     ),
                   ),
                 ),
                 Center(
-                  child: Text(micText),
+                  child: Text(_speech.isListening ? micList : micText),
                 ),
               ],
             ),
